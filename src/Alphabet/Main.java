cb9bbc5c861d0f40366b44502e2a1cbf79c9b4bb
Pantiukhov Roman <pantiukhov.r@gmail.com>
2022-01-18 21:52:56 +0300

    Studying in progress! Encapsulation!

---
+++ src/Alphabet/Main.java
@@ -0,0 +1,11 @@
+package Alphabet;
+
+public class Main {
+    public static void main(String[] args) {
+        Alphabet alphabet = new Alphabet();
+        alphabet.setY(5);
+        alphabet.setS(7);
+        alphabet.setD(7);
+        System.out.println(alphabet.getSum());
+    }
+}
